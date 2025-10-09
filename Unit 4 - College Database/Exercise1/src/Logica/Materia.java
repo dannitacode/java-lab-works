2cba1fd7c9aff818d40414e0137ce21fd2d84aa3
dannitacode <dannavalenchu076@gmail.com>
2025-10-08 23:53:30 -0300

    Modificando JIF de Materia para dar lugar a su visualización. Mínimos cambios en MateriaData

---
+++ Unit 4 - College Database/Exercise1/src/Logica/Materia.java
@@ -0,0 +1,52 @@
+package Logica;
+
+/**
+ *
+ * @author dannita
+ */
+public class Materia {
+    
+    private int id;
+    private String nombre;
+    private int año;
+    private boolean estado;
+
+    public Materia(String nombre, int año, boolean estado) {
+        this.nombre = nombre;
+        this.año = año;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+}
---
+++ Unit 4 - College Database/Exercise1/src/Persistencia/MateriaData.java
@@ -0,0 +1,212 @@
+package Persistencia;
+
+import Logica.Materia;
+import java.sql.*;
+
+/**
+ *
+ * @author dannita
+ */
+public class MateriaData {
+
+    public void ingresarMateria(Materia materia) {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "INSERT INTO materia (id_materia, nombre, año, estado) VALUES (?,?,?,?)";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, materia.getId());
+            ps.setString(2, materia.getNombre());
+            ps.setInt(3, materia.getAño());
+            ps.setBoolean(4, materia.isEstado());
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Materia agregada exitosamente.");
+            }
+        } catch (SQLException s) {
+            System.out.println("Error: No se pudo procesar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void mostrarMateria() {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "SELECT * FROM materia";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Materia materia = new Materia(
+                        rs.getString("nombre"),
+                        rs.getInt("año"),
+                        rs.getBoolean("estado")
+                );
+                materia.setId(rs.getInt("id_materia"));
+                System.out.println("Id materia: " + materia.getId() + "\nNombre: " + materia.getNombre() + "\nAño: " + materia.getAño() + "\nEstado: " + (materia.isEstado() ? "Activa" : "Inactiva"));
+            }
+        } catch (SQLException s) {
+            System.out.println("Error: No se pudo procesar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void actualizarMateria(int id, int año) {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "UPDATE materia SET año=? WHERE id=?";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, año);
+            ps.setInt(2, id);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Materia actualizada exitosamente.");
+            } else {
+                System.out.println("No existe esa materia con ese ID.");
+            }
+        } catch (SQLException s) {
+            System.out.println("Error: No se pudo realizar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void buscarMateria(int id) {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "SELECT * FROM materia WHERE id=?";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Materia materia = new Materia(
+                        rs.getString("nombre"),
+                        rs.getInt("año"),
+                        rs.getBoolean("estado")
+                );
+                materia.setId(rs.getInt("id"));
+                System.out.println("Materia encontrada.");
+                System.out.println("Id materia: " + materia.getId() + "\nNombre: " + materia.getNombre() + "\nAño: " + materia.getAño() + "\nEstado: " + (materia.isEstado() ? "Activa" : "Inactiva"));
+            } else {
+                System.out.println("La materia con ese ID no fue encontrada.");
+            }
+        } catch (SQLException s) {
+            System.out.println("Error: No se pudo realizar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void bajaLogica(int id) {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "UPDATE materia SET estado=0 WHERE id=?";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Materia dada de baja exitosamente");
+            } else {
+                System.out.println("No existe una materia con ese ID.");
+            }
+        } catch (SQLException s) {
+            System.out.println("No se pudo procesar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void altaLogica(int id) {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "UPDATE materia SET estado=1 WHERE id=?";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Materia dada de alta exitosamente");
+            } else {
+                System.out.println("No existe una materia con ese ID.");
+            }
+        } catch (SQLException s) {
+            System.out.println("No se pudo procesar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void borrarMateria(int id) {
+        Connection conexion = null;
+        try {
+            conexion = DbConexion.establecerConexion();
+            String query = "DELETE FROM materia WHERE id_materia = ?";
+            PreparedStatement ps = conexion.prepareStatement(query);
+            ps.setInt(1, id);
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Materia borrada exitosamente.");
+            } else {
+                System.out.println("No se encontró una materia con ese ID.");
+            }
+        } catch (SQLException s) {
+            System.out.println("No se pudo procesar la consulta.");
+            s.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException s) {
+                    s.printStackTrace();
+                }
+            }
+        }
+    }
+}
